40793f10ca04cd94b582f999284dbb30d752a158
christopherhanke <christopherhanke@googlemail.com>
2020-11-02 20:24:29 +0100

    first commit. initializing flask and base-html

---
+++ main.py
@@ -0,0 +1,32 @@
+import uuid
+import hashlib
+
+from flask import Flask, render_template, redirect, url_for, request
+from datetime import datetime
+from models import db, User, Messages
+
+
+# runtime environment
+app = Flask(__name__)
+app.secret_key = str(uuid.uuid4())
+db.create_all()
+
+
+# index page
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,21 @@
+import os
+from sqla_wrapper import SQLAlchemy
+
+db = SQLAlchemy(os.getenv("DATABASE_URL", "sqlite:///localhost.sqlite"))
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    email = db.Column(db.String(100), unique=True)
+    location = db.Column(db.String)
+    password = db.Column(db.String)
+    deleted = db.Column(db.Boolean)
+
+
+class Messages(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String)
+    time = db.Column(db.String)
+    sender = db.Column(db.String)
+    receiver = db.Column(db.String)
